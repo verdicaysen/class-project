a28f26add8b3cdce048fddd6e6b756becc2baa40
rashley <verdicaysen@gmail.com>
2024-09-24 18:53:24 -0500

    Working on dictionaries.

--- Chapter 3/data_structs.gd
+++ Chapter 3/data_structs.gd
@@ -44,5 +44,25 @@ func _ready() -> void:
 	#Let's do the numbers range of 10 to 20.
 	for number in range(10, 20):
 		print(number)
+	while inventory.size() > 3:
+		inventory.remove_at(0)
+		print(inventory[0])
+		
+	# Establish new array for continue and break practice.
+	var new_inventory: Array = ["Boots", "Bandages", "Bananas", "Warm Gloves", "Goggles"]
+	while new_inventory.size() > 3:
+		if new_inventory[0] == "Bananas":
+			break
+		print("Removing :", new_inventory[0])
+		new_inventory.remove_at(0)
 	
-	
+	# Dictionary. Looks like you can use different value types. Might also be able to force in files. Will research more.
+	var item: Dictionary = {
+		"name"  : "Shoes",
+		"price" : 2,
+		"weight": 2
+	}
+	# We can print the contents of this item and then we can increment it by a new amount and then view the change.
+	print(item)
+	item["price"] += 10
+	print(item["price"])
